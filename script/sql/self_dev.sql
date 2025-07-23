97abbcc9cd0d87f9917eb0d7ad18ac61558892f4
James LIm <james.lim@atoz-software.tech>
2025-07-23 09:20:26 +0800

    add script

--- script/sql/self_dev.sql
+++ script/sql/self_dev.sql
@@ -9,3 +9,13 @@ create table sub_acc_usr_perm
 
 ALTER TABLE sys_user
 ADD COLUMN parent_id VARCHAR(50);
+
+
+CREATE TABLE IF NOT EXISTS `each_usr_perm` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `username` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `menu_id` varchar(3000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `perm_list` varchar(3000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `tenant_id` varchar(20) DEFAULT '000000' COMMENT '租户编号',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
