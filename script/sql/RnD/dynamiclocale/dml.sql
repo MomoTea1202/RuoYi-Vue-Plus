2ecd779d4044bc31563fbf3be99799e8213883b3
James LIm <james.lim@atoz-software.tech>
2025-07-25 09:18:53 +0800

    revise search criteria

--- script/sql/RnD/dynamiclocale/dml.sql
+++ script/sql/RnD/dynamiclocale/dml.sql
@@ -1,3 +1,23 @@
-INSERT INTO `sys_locale` (`id`, `locale`, `code`, `value`) VALUES
-	(5, 'en-US', 'system.title', 'EzPay'),
-	(6, 'zh-CN', 'system.title', '简单还');
+INSERT INTO `sys_locale` ( `locale`, `code`, `value`) VALUES
+	( 'en-US', 'system.title', 'EzPay'),
+	( 'zh-CN', 'system.title', '亿支付'),
+	( 'en-US', 'page.login.pwdLogin.rememberMe', 'remember me '),
+    ( 'zh-CN', 'page.login.pwdLogin.rememberMe', '记住我'),
+    ( 'en-US', 'page.login.common.userNamePlaceholder', 'Login Id'),
+    ( 'zh-CN', 'page.login.common.userNamePlaceholder', '登录账号'),
+    ( 'en-US', 'page.login.common.passwordPlaceholder', 'Password '),
+    ( 'zh-CN', 'page.login.common.passwordPlaceholder', '密码'),
+    ( 'en-US', 'page.login.pwdLogin.forgetPassword', 'Forget Password'),
+    ( 'zh-CN', 'page.login.pwdLogin.forgetPassword', '忘记密码'),
+    ( 'en-US', 'common.login', 'Login '),
+    ( 'zh-CN', 'common.login', '登录')，
+    ( 'en-US', 'form.userName.required', 'Username Are Require'),
+    ( 'zh-CN', 'form.userName.required', '账号必须填'),
+    ( 'en-US', 'form.pwd.required', 'Password Are Require '),
+    ( 'zh-CN', 'form.pwd.required', '密码必须填'),
+    ( 'en-US', 'page.login.pwdLogin.loginToYourAcc', 'Login To Account '),
+    ( 'zh-CN', 'page.login.pwdLogin.loginToYourAcc', '登录到您的账户'),
+    ( 'en-US', 'page.login.pwdLogin.welcomeToLogin', 'Welcome Back！ Please Fill In Your Account Information '),
+    ( 'zh-CN', 'page.login.pwdLogin.welcomeToLogin', '欢迎回来！请输入您的账户信息')
+
+
