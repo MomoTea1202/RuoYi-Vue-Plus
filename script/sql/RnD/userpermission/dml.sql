f8ad702d239a8cf2f9edafc2d9e93bb692a9097c
James LIm <james.lim@atoz-software.tech>
2025-08-22 17:42:54 +0800

    add locale

---
+++ script/sql/RnD/userpermission/dml.sql
@@ -0,0 +1,7 @@
+INSERT INTO `sys_locale` ( `locale`, `code`, `value`) VALUES
+	( 'en-US', 'route.userPerm', 'User Permission'),
+	( 'zh-CN', 'route.userPerm', '用戶权限'),
+	( 'en-US', 'route.subAcc', 'Sub Account'),
+    ( 'zh-CN', 'route.subAcc', '子账号');
+
+
