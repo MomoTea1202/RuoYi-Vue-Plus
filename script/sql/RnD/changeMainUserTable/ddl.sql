77a10ec54cacd3a668b1a73a8ffd931332a0d16c
James LIm <james.lim@atoz-software.tech>
2025-08-25 10:19:35 +0800

    remove the dept and position script

--- script/sql/RnD/changeMainUserTable/ddl.sql
+++ script/sql/RnD/changeMainUserTable/ddl.sql
@@ -1,1 +1,5 @@
 ALTER TABLE sys_user DROP COLUMN dept_id ;
+DROP TABLE IF EXISTS `sys_user_post`;
+DROP TABLE IF EXISTS `sys_role_dept`;
+DROP TABLE IF EXISTS `sys_post`;
+DROP TABLE IF EXISTS `sys_dept`;
